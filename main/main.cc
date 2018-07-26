61bf6b3dcab4c2edf00f31b3cd0226db9ce62421
adam-szymanski <to.adam.szymanski@gmail.com>
2018-07-26 23:17:40 +0200

    Clean main

--- main/main.cc
+++ main/main.cc
@@ -5,12 +5,5 @@ using namespace std;
 using namespace Zerg;
 
 int main() {
-    Tensor t({4});
-    t.val(0) = 0;
-    t.val(1) = 1;
-    t.val(2) = 2;
-    t.val(3) = 3;
-    cout << "t=" << t;
-    
     return 0;
 }
